6bda439a122f227d9f34f607ee96a9bacdc43d52
Muhammet Canbaz <muhammet.canbaz@nisantasi.edu.tr>
2021-01-17 00:11:17 +0300

    16 Ocak ders sonu.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1 ??
+
+
+            int[] sayyilar1 = new int[] { 10, 20, 30 };
+            int[] sayyilar2 = new int[] { 10, 20, 30 };
+            sayyilar1 = sayyilar2;
+            sayyilar2[0] = 000;
+            //sayilar[0]
+        }
+
+        //int,double, float ----- değer tiplerdir.
+        //class, arrayler ----- referans tiplerdir
+        //
+
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //do not repeat yourself = kendini tekrarlama
+            //kategoriEtiketi değer tutucu, aliasdır. Artık bizim için değeri var.
+
+            string kategoriEtiketi = "Kategori";
+            int ogranciSayisi = 320000000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true; //True ve False verilir.
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            //bool örneği
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
