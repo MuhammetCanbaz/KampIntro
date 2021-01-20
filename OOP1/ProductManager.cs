6c4f4b7df350d9d79c99af024c61d5a1bd9b9610
Muhammet Canbaz <muhammet.canbaz@nisantasi.edu.tr>
2021-01-20 23:50:36 +0300

    20 ocak ders sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product) 
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi.");
+        }
+
+               
+        }
+    }
+}
+//void olayı şöyle düşünülebilir. Emir kipi ile çağırıyoruz ve işlemi yapıyor. 
+//return'de ise elde edilecek sonuç tekrar kullanılacak ise return komutu kullanılır.
